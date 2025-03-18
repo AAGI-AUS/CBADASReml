7cd592bdcea3c12065aea62f675165b0ed62bc31
kaibagley <kaibagley@pm.me>
2025-03-18 16:02:22 +0800

    Add some unit tests

---
+++ tests/testthat/test-lsd_group.R
@@ -0,0 +1,74 @@
+invisible(capture.output(
+    model <- asreml(
+        fixed = yield ~ Variety,
+        random = ~units,
+        residual = ~ ar1v(Row):ar1(Column),
+        data = shf,
+        trace = FALSE
+    )
+))
+
+test_that("groups are same as agricolae::orderPvalue", {
+    agricolae_groups <- c(
+        "a",
+        "a",
+        "ab",
+        "abc",
+        "abcd",
+        "abcde",
+        "bcde",
+        "bcdef",
+        "bcdef",
+        "bcdef",
+        "cdef",
+        "defg",
+        "defg",
+        "defg",
+        "efgh",
+        "fghi",
+        "fghi",
+        "ghij",
+        "hijk",
+        "ijkl",
+        "ijkl",
+        "ijkl",
+        "jkl",
+        "kl",
+        "l"
+    )
+
+    classify <- "Variety"
+
+    capture.output(
+        pred <- asremlPlus::predictPlus.asreml(
+            model,
+            classify = classify,
+            wald.tab = as.data.frame(
+                asreml::wald(model, denDF = "algebraic")$Wald
+            )
+        )
+    )
+
+    response <- model[["call"]][["fixed"]][[2]]
+    response <- toString(response)
+
+    # LSD Value
+    lsd <- pred$LSD$assignedLSD
+
+    prob.matrix <- ifelse(is.na(pred$p.differences), 1, pred$p.differences)
+
+    treatments <- colnames(prob.matrix)
+    means <- pred$predictions$predicted.value
+
+    alpha <- 0.05
+
+    lsdmeantab <-
+        lsd_group(
+            treatments,
+            means,
+            alpha,
+            prob.matrix
+        )
+
+    expect_equal(lsdmeantab$group, agricolae_groups)
+})
---
+++ tests/testthat/test-lsd_table.R
@@ -0,0 +1,101 @@
+model <- asreml(
+    fixed = yield ~ Variety,
+    random = ~units,
+    residual = ~ ar1v(Row):ar1(Column),
+    data = shf,
+    trace = FALSE
+)
+
+test_that("table matches output as if we used agricolae::orderPvalue", {
+    agricolae_table <-
+        data.frame(
+            groups = c(
+                "a",
+                "a",
+                "ab",
+                "abc",
+                "abcd",
+                "abcde",
+                "bcde",
+                "bcdef",
+                "bcdef",
+                "bcdef",
+                "cdef",
+                "defg",
+                "defg",
+                "defg",
+                "efgh",
+                "fghi",
+                "fghi",
+                "ghij",
+                "hijk",
+                "ijkl",
+                "ijkl",
+                "ijkl",
+                "jkl",
+                "kl",
+                "l"
+            ),
+            treatments = c(
+                "19",
+                "20",
+                "13",
+                "22",
+                "25",
+                "24",
+                "18",
+                "6",
+                "2",
+                "21",
+                "17",
+                "5",
+                "15",
+                "8",
+                "12",
+                "4",
+                "3",
+                "7",
+                "11",
+                "23",
+                "14",
+                "16",
+                "9",
+                "1",
+                "10"
+            ),
+            lsds = c(
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256,
+                120.5256
+            )
+        )
+
+    lt <- lsd_table(model, "Variety")
+    lt$lsds <- round(lt$lsds, 4)
+
+    expect_equal(lt$groups, agricolae_table$groups)
+    expect_equal(lt$treatments, agricolae_table$treatments)
+    expect_equal(lt$lsds, agricolae_table$lsds)
+})
