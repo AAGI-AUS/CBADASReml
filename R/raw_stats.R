9afe5b7bdf0381b3482a2e8dca09f6ec06dff479
kaibagley <kaibagley@pm.me>
2025-03-20 16:13:44 +0800

    rewrite without dependencies

--- R/raw_stats.R
+++ R/raw_stats.R
@@ -1,41 +1,59 @@
 #' Produces a summary table for the data with Mean, Median, SD, CV and Range.
 #'
 #' This function outputs the summary statistics of the given data
-#' @param data, a data set to be summarised
-#' @param response, variable (without quotes) to get summary statistics of.
-#' @param decimal_places, number of decimal places to show
-#' @param ... variables (without quotes) to group and arrange the data in.
+#' @param data a data set to be summarised
+#' @param response string variable name in the data of the response
+#' @param groupby vector of strings of the names of the grouping variables in
+#'     the data
 #'
-#' @keywords Raw statistics
-#' @import dplyr
-#'
-#' @return a data.frame
+#' @return data.frame
 #' @export
-#' @examples \dontrun{
-#' exploratory_table(mtcars, disp, cyl, gear)
-#' }
-exploratory_table <- function(data, response, decimal_places = 3, ...) {
-    resp <- enquo(resp)
+#'
+#' @examples
+#' exploratory_table(mtcars, response = "mpg", groupby = c("cyl", "gear"))
 
-    table <- data %>%
-        group_by(!!!rlang::ensyms(...)) %>%
-        mutate(
-            Mean = mean(!!resp, na.rm = T),
-            Median = median(!!resp, na.rm = T),
-            SD = stats::sd(!!resp, na.rm = T),
-            CV = 100 * stats::sd(!!resp, na.rm = T) / mean(!!resp, na.rm = T),
-            Reps = n(),
-            Range = paste0(
-                round(range(!!resp, na.rm = T)[1], decimal_places),
-                " - ",
-                round(range(!!resp, na.rm = T)[2], decimal_places)
-            )
-        ) %>%
-        distinct(!!!rlang::ensyms(...), Mean, Median, SD, CV, Range, Reps) %>%
-        arrange(!!!rlang::ensyms(...)) %>%
-        mutate(across(c(Mean, Median, SD, CV), ~ round(.x, decimal_places))) %>%
-        select(!!!rlang::ensyms(...), Reps, Range, Mean, Median, SD, CV) %>%
-        ungroup()
+exploratory_table <- function(data, response, groupby) {
+    if (!response %in% names(data)) {
+        stop("response must be a column in the data")
+    }
+    if (!all(groupby %in% names(data))) {
+        stop("grouping variables must be columns in the data")
+    }
 
-    return(table)
+    compute_stats <- function(x) {
+        mn <- mean(x, na.rm = TRUE)
+        med <- median(x, na.rm = TRUE)
+        sd <- sd(x, na.rm = TRUE)
+        cv <- 100 * sd / mn
+        reps <- length(x)
+        rnge <- range(x, na.rm = TRUE)
+
+        return(c(
+            mean = mn,
+            median = med,
+            sd = sd,
+            cv = cv,
+            reps = reps,
+            min = rnge[1],
+            max = rnge[2],
+            range = paste(rnge, collapse = " - ")
+        ))
+    }
+
+    # Use aggregate to compute statistics per group
+    aggregated_data <- aggregate(
+        x = data[[response]],
+        by = data[groupby],
+        FUN = \(x) list(compute_stats(x))
+    )
+
+    # Extract computed statistics and unnest data frames
+    stats_list <- do.call(rbind, aggregated_data$x)
+    summary_df <- cbind(aggregated_data[groupby], stats_list)
+
+    # Order by grouping variables
+    summary_df <- summary_df[do.call(order, summary_df[groupby]), ]
+    rownames(summary_df) <- NULL
+
+    return(summary_df)
 }
