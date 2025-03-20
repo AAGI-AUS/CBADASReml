149e9e298b7cd4289c31de34fd1416a448d29fe2
kaibagley <kaibagley@pm.me>
2025-03-20 13:56:00 +0800

    fix parameter name

--- R/raw_stats.R
+++ R/raw_stats.R
@@ -2,20 +2,21 @@
 #'
 #' This function outputs the summary statistics of the given data
 #' @param data, a data set to be summarised
-#' @param response, variable (without quotes) to get summary statistics of. 
-#' @param ... variables (without quotes) to group and arrange the data in.  
+#' @param response, variable (without quotes) to get summary statistics of.
 #' @param decimal_places, number of decimal places to show
+#' @param ... variables (without quotes) to group and arrange the data in.
+#'
 #' @keywords Raw statistics
 #' @import dplyr
-#' @export
+#'
 #' @return a data.frame
+#' @export
 #' @examples \dontrun{
 #' exploratory_table(mtcars, disp, cyl, gear)
 #' }
-exploratory_table <- function(data, resp, ..., decimal_places = 3) {
-    
+exploratory_table <- function(data, response, decimal_places = 3, ...) {
     resp <- enquo(resp)
-    
+
     table <- data %>%
         group_by(!!!rlang::ensyms(...)) %>%
         mutate(
@@ -25,16 +26,16 @@ exploratory_table <- function(data, resp, ..., decimal_places = 3) {
             CV = 100 * stats::sd(!!resp, na.rm = T) / mean(!!resp, na.rm = T),
             Reps = n(),
             Range = paste0(
-                round(range(!!resp, na.rm = T)[1], decimal_places), " - ",
+                round(range(!!resp, na.rm = T)[1], decimal_places),
+                " - ",
                 round(range(!!resp, na.rm = T)[2], decimal_places)
             )
         ) %>%
         distinct(!!!rlang::ensyms(...), Mean, Median, SD, CV, Range, Reps) %>%
         arrange(!!!rlang::ensyms(...)) %>%
         mutate(across(c(Mean, Median, SD, CV), ~ round(.x, decimal_places))) %>%
-        select(!!!rlang::ensyms(...), Reps, Range, Mean, Median, SD, CV) %>% 
+        select(!!!rlang::ensyms(...), Reps, Range, Mean, Median, SD, CV) %>%
         ungroup()
-    
+
     return(table)
 }
- 
