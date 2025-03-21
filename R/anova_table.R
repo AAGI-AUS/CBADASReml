d2bd37ab21ce06bc12617555b82ef98323965c13
kaibagley <kaibagley@pm.me>
2025-03-21 14:36:33 +0800

    remove dependencies and add unit testing

--- R/anova_table.R
+++ R/anova_table.R
@@ -3,13 +3,16 @@
 #' This function allows you to observe the anova table for multiple ASReml
 #' Models. It outputs a table as well as a printed xtable to the console.
 #' @param ... `asreml`, any number of asreml models.
-#' @param ndig `numeric`, number of digits to round results to.
+#' @param n_digits `numeric`, number of digits to round results to.
+#'
+#' @return a dataframe containing all anova tables. Can be used with xtable to
+#'     produce a LaTeX table.
 #' @export
-#' @return a dataframe containing all anova tables
+#'
 #' @examplesIf requireNamespace("asreml", quietly = TRUE)
 #' library(asreml)
 #' test_data <- oats
-#' test_data["yield2"] <- test_data["yield"] * runif(nrow(test_data["yield"]))
+#' test_data["yield2"] <- oats["yield"] * runif(nrow(oats["yield"]))
 #' mod1 <- asreml(
 #'     fixed = yield ~ Variety + Nitrogen + Variety:Nitrogen,
 #'     random = ~idv(Blocks) + idv(Blocks):idv(Wplots),
@@ -22,38 +25,85 @@
 #'     residual = ~idv(units),
 #'     data = test_data
 #' )
-#' anova_table(mod1, mod2, ndig = 5)
-anova_table <- function(..., ndig = 3) {
-    models <- list(...)
+#' anova_table(mod1, mod2)
 
-    response_names <- unlist(
-        lapply(models, function(x) toString(x[["call"]][["fixed"]][[2]]))
-    )
-
-    if (length(unique(response_names)) != length(models)) {
-        stop(paste("Models should have different response names:", response_names))
+anova_table <- function(..., n_digits = 3) {
+    if (...length() == 0) {
+        stop("No models passed to this function")
+    }
+    if (!is.numeric(n_digits)) {
+        stop(paste0("n_digits must be numeric: ", n_digits))
     }
 
-    wald_list <- lapply(models, function(mod) {
-        as.data.frame(asreml::wald.asreml(mod))
-    })
+    models <- list(...)
+    if (!all(sapply(models, \(x) inherits(x, "asreml")))) {
+        stop("All given dots (...) parameters must be asreml models")
+    }
 
-    significance_list <- lapply(wald_list, function(x) x["Pr(Chisq)"])
+    response_names <- sapply(models, \(x) toString(x[["call"]][["fixed"]][[2]]))
 
-    pval_table <- significance_list %>%
-        purrr::map(function(x) tibble::rownames_to_column(x, "rn")) %>%
-        purrr::reduce(full_join, by = "rn")
-
-    colnames(pval_table) <- c("Source of Variation", response_names)
-
-    pval_table <- pval_table %>%
-        filter(
-            !(`Source of Variation` %in% c("(Intercept)", "residual (MS)"))
-        ) %>%
-        mutate(across(
-            all_of(response_names),
-            ~ round(.x, digits = ndig)
+    if (length(unique(response_names)) != length(models)) {
+        stop(paste(
+            "Models should have different response names:",
+            response_names
         ))
-    print(xtable::xtable(pval_table), include.rownames = F)
+    }
+
+    wald_list <- lapply(
+        models,
+        \(mod) as.data.frame(asreml::wald.asreml(mod))
+    )
+
+    sig_list <- lapply(
+        wald_list,
+        \(x)
+            data.frame(
+                Effect = rownames(x),
+                pvalue = as.numeric(x[, "Pr(Chisq)"]),
+                row.names = NULL
+            )
+    )
+
+    ## lapply returns list of dfs, which is reduced by full outer joins
+    pval_table <- Reduce(
+        \(x, y) merge(x, y, by = "Effect", all = TRUE),
+        sig_list
+    )
+
+    ## Return to original order
+    pval_table <- pval_table[match(sig_list[[1]]$Effect, pval_table$Effect), ]
+
+    ## pval_table <-
+    ##     sig_list %>%
+    ##     purrr::map(\(x) tibble::rownames_to_column(x, "coef")) %>%
+    ##     purrr::reduce(full_join, by = "coef")
+
+    names(pval_table) <- c("Effect", response_names)
+
+    ## Filter out intercept and residual pvalues
+    pval_table <- pval_table[
+        !(pval_table[["Effect"]] %in%
+            c("(Intercept)", "residual (MS)")),
+    ]
+
+    ## Round pvals to n_digits
+    pval_table[, response_names] <-
+        lapply(
+            pval_table[, response_names, drop = FALSE],
+            \(x) round(x, digits = n_digits)
+        )
+
+    rownames(pval_table) <- NULL
+
+    ## pval_table1 <-
+    ##     pval_table %>%
+    ##     filter(
+    ##         !(`Source of Variation` %in% c("(Intercept)", "residual (MS)"))
+    ##     ) %>%
+    ##     mutate(across(
+    ##         all_of(response_names),
+    ##         \(x) round(x, digits = n_digits)
+    ##     ))
+
     return(pval_table)
 }
---
+++ tests/testthat/test-anova_table.R
@@ -0,0 +1,77 @@
+set.seed(123)
+test_data <- oats
+test_data["yield2"] <- oats["yield"] * runif(nrow(oats["yield"]))
+test_data["yield3"] <- oats["yield"] * runif(nrow(oats["yield"])) * 2
+
+mod1 <- asreml(
+    fixed = yield ~ Variety + Nitrogen + Variety:Nitrogen,
+    random = ~ idv(Blocks) + idv(Blocks):idv(Wplots),
+    residual = ~ idv(units),
+    data = test_data,
+    trace = FALSE
+)
+mod2 <- asreml(
+    fixed = yield2 ~ Variety + Nitrogen + Variety:Nitrogen,
+    random = ~ idv(Blocks) + idv(Blocks):idv(Wplots),
+    residual = ~ idv(units),
+    data = test_data,
+    trace = FALSE
+)
+mod3 <- asreml(
+    fixed = yield3 ~ Variety + Nitrogen + Variety:Nitrogen,
+    random = ~ idv(Blocks) + idv(Blocks):idv(Wplots),
+    residual = ~ idv(units),
+    data = test_data,
+    trace = FALSE
+)
+out <- data.frame(
+    Effect = c("Variety", "Nitrogen", "Variety:Nitrogen"),
+    yield = c(0.226, 0.000, 0.936)
+)
+
+## Tests
+test_that("appropriate errors are given for incorrect parameters given", {
+    ## stop("No models passed to this function")
+    expect_error(
+        anova_table(),
+        "^No models passed.*$"
+    )
+
+    ## stop(paste0("n_digits must be numeric: ", n_digits))
+    expect_error(
+        anova_table(mod1, n_digits = "rigatoni"),
+        "^n_digits.*rigatoni$"
+    )
+
+    ## stop("All given dots (...) parameters must be asreml models")
+    expect_error(
+        anova_table("spaghetti"),
+        "^All given dots.*$"
+    )
+})
+
+test_that("one model works", {
+    expect_equal(anova_table(mod1), out)
+})
+test_that("two models work", {
+    out2 <- cbind(out, yield2 = c(0.059, 0.004, 0.562))
+    expect_equal(anova_table(mod1, mod2), out2)
+})
+test_that("two+ models work", {
+    out3 <- cbind(
+        out,
+        yield2 = c(0.059, 0.004, 0.562),
+        yield3 = c(0.978, 0.000, 0.171)
+    )
+    expect_equal(anova_table(mod1), out)
+})
+
+test_that("n_digits returns correct digits", {
+    real_num <- anova_table(mod1, n_digits = 10)$yield
+    test_num <- anova_table(mod1)$yield
+    expect_true(all(test_num == round(real_num, 3)))
+
+    test_nd <- 5
+    test_num <- anova_table(mod1, n_digits = test_nd)$yield
+    expect_true(all(test_num == round(real_num, test_nd)))
+})
