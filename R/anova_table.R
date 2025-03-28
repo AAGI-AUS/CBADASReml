37f3e1772020c59bd5a60e732252509654b952fc
kaibagley <kaibagley@pm.me>
2025-03-28 16:31:27 +0800

    add glmmTMB method from Matt's code

--- R/anova_table.R
+++ R/anova_table.R
@@ -26,21 +26,22 @@
 #'     data = test_data
 #' )
 #' anova_table(mod1, mod2)
-
 anova_table <- function(..., n_digits = 3) {
-    if (...length() == 0) {
-        stop("No models passed to this function")
-    }
-    if (!is.numeric(n_digits)) {
-        stop(paste0("n_digits must be numeric: ", n_digits))
-    }
+    UseMethod("anova_table")
+}
 
+#' @method anova_table asreml
+#' @export
+anova_table.asreml <- function(..., n_digits = 3) {
     models <- list(...)
     if (!all(sapply(models, \(x) inherits(x, "asreml")))) {
         stop("All given dots (...) parameters must be asreml models")
     }
 
-    response_names <- sapply(models, \(x) toString(x[["call"]][["fixed"]][[2]]))
+    response_names <- sapply(
+        models,
+        \(x) as.character(x[["call"]][["fixed"]][[2]])
+    )
 
     if (length(unique(response_names)) != length(models)) {
         stop(paste(
@@ -71,12 +72,9 @@ anova_table <- function(..., n_digits = 3) {
     )
 
     ## Return to original order
-    pval_table <- pval_table[match(sig_list[[1]]$Effect, pval_table$Effect), ]
-
-    ## pval_table <-
-    ##     sig_list %>%
-    ##     purrr::map(\(x) tibble::rownames_to_column(x, "coef")) %>%
-    ##     purrr::reduce(full_join, by = "coef")
+    pval_table <- pval_table[
+        match(sig_list[[1]]$Effect, pval_table$Effect),
+    ]
 
     names(pval_table) <- c("Effect", response_names)
 
@@ -95,15 +93,72 @@ anova_table <- function(..., n_digits = 3) {
 
     rownames(pval_table) <- NULL
 
-    ## pval_table1 <-
-    ##     pval_table %>%
-    ##     filter(
-    ##         !(`Source of Variation` %in% c("(Intercept)", "residual (MS)"))
-    ##     ) %>%
-    ##     mutate(across(
-    ##         all_of(response_names),
-    ##         \(x) round(x, digits = n_digits)
-    ##     ))
+    return(pval_table)
+}
+
+#' @method anova_table glmmTMB
+#' @export
+anova_table.glmmTMB <- function(..., n_digits = 3, zi = FALSE) {
+    models <- list(...)
+
+    response_names <- sapply(
+        models,
+        \(x)
+            as.character(attr(
+                x$modelInfo$reTrms$cond$terms$fixed,
+                "variables"
+            )[[2]])
+    )
+
+    anova_list <- lapply(
+        models,
+        \(mod) {
+            as.data.frame(
+                glmmTMB:::Anova.II.glmmTMB(
+                    mod,
+                    component = ifelse(zi, "zi", "cond")
+                )
+            )
+        }
+    )
+
+    sig_list <- lapply(
+        anova_list,
+        \(x)
+            data.frame(
+                Effect = rownames(x),
+                pvalue = as.numeric(x[, "Pr(>Chisq)"]),
+                row.names = NULL
+            )
+    )
+
+    ## lapply returns list of dfs, which is reduced by full outer joins
+    pval_table <- Reduce(
+        \(x, y) merge(x, y, by = "Effect", all = TRUE),
+        sig_list
+    )
+
+    ## Return to original order
+    pval_table <- pval_table[
+        match(sig_list[[1]]$Effect, pval_table$Effect),
+    ]
+
+    names(pval_table) <- c("Effect", response_names)
+
+    ## Filter out intercept and residual pvalues
+    pval_table <- pval_table[
+        !(pval_table[["Effect"]] %in%
+            c("(Intercept)", "residual (MS)")),
+    ]
+
+    ## Round pvals to n_digits
+    pval_table[, response_names] <-
+        lapply(
+            pval_table[, response_names, drop = FALSE],
+            \(x) round(x, digits = n_digits)
+        )
+
+    rownames(pval_table) <- NULL
 
     return(pval_table)
 }
